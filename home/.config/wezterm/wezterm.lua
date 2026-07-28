28c373eff2216baf0e3cc467879be835ce9a10fd
burakgizlice <burakgizlice114@gmail.com>
2026-07-28 15:30:41 +0300

    wezterm: add fullscreen toggle keybindings (Super+Enter, F11)

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -9,5 +10,18 @@ config.window_background_opacity = 0.8
 -- config.macos_window_background_blur = 50  -- macOS-only; no-op on Linux
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+
+-- Toggle fullscreen with Super+Enter (or press F11)
+config.keys = {
+  {
+    key = "Return",
+    mods = "SUPER",
+    action = act.ToggleFullScreen,
+  },
+  {
+    key = "F11",
+    action = act.ToggleFullScreen,
+  },
+}
 
 return config
